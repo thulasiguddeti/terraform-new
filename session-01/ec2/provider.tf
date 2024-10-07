568e87f60a1175cb3edd0ce5be8594db97899bdf
thulasiguddeti <thulasiguddeti02@gmail.com>
2024-10-07 11:54:23 +0530

    Terraform

--- session-01/ec2/provider.tf
+++ session-01/ec2/provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.69.0" # 
+      version = "5.69.0" # AWS provider version, not terraform version
     }
   }
 }
 
 provider "aws" {
   # Configuration options
+  region = "us-east-1"
 }
